2b11ec25d2a39c303941c40cf17fe199f2d1cee0
Michael Odumosu <michaelodumosu57@gmail.com>
2017-12-17 20:12:16 -0500

    learned about computer history, hardware and software, history was very interesting

---
+++ learning_about_your_computer.py
@@ -0,0 +1,109 @@
+learning about how a computer works
+
+
+    Computer Types
+    
+        Analogue computers- render output about changing physical values
+        Digital computers- use binary or number input and use programs to learn about them
+        Hybrid Computer- is the best of both worlds
+        
+        Supercomputer- process trillions of instructions thousands of interconnected processors
+        Mainframe computer - for the use of 100's of user, for banking and commercial use
+        Miniframe computer - used for employess to keep track of goods
+        Workstation - a computer specialized for producing products
+        Microcomputer- your everyday computer
+        
+    Computer Components
+    
+        input - use of input devices to enter raw info to the computer
+        store - primary and secondary storage that holds data and instruction before send to the Central Processing Unit and before displaying it as output
+        process - turning into useful info, Computer process displays and sends its back to storage as useful information
+        output - presenting data through output devices
+        controll- makes sure everything is done correctly
+        
+        input devices-
+        
+            keyboard- uses keys, common QWERTY is the common one
+            mouse- controls movement of cursor and screen posiition
+            scanners - uses pictures or text as input, and turns digital, flatbed scanners are input
+            joystick - it is for more game like use and it controls the cursor
+            lightpen - for signinig things. Reacts by hitting the screen, good for drawing
+            trackball -used when do not have enough room for mouse, senses movement of ball
+            digitizer - screen that comes with stylus, allows user to draw detailed images
+            microphone- turns sound into digitial data stored in the
+            Magnetic Ink Character Recognition (MICR)- process the ink found on checks
+            Optical Character Reader (OCR) - a mini scaners takes, the objects or texts it sees and records it
+            
+        output devices-
+        
+            monitors
+            
+                CRT monitor - CRT monitors are based on the cathode ray tubes. Cathode rays tube produces beam of electrons that strikes on the inner phosphorescent surface of screen to produce images on the screen.
+                
+                LCD monitor - has two screens with liquid crystal between them, when light passes, the crystal alters it for the final color output using cold cathode flourescent
+                
+                LED monitor - does same thing as LCD but uses light emitting diodes
+                
+                PLASMA sCREEN- uses plasma gas and by applying energy, the plasma changes color
+                
+            printers-
+            
+                Impact printers- uses a hammer to strike an ink ribbon to print paper
+                
+                    Character printers - does this one character at a time
+                    
+                    Dot matrix printers - done with a matrix of dots
+                    
+                    daisy wheel printer - looks for characters on wheel, then takes the paper offset to that character and the character is printer
+                    
+                    
+        
+        Central Processing Unit
+        
+            process for everything in the computer
+            stores important OS programs
+            help input and output devices to communicate with each other
+                    
+                    
+            has 3 parts
+            
+                Memory: It is called Random access memory (RAM). It temporarily stores data, programs and intermediate and final results of processing.
+                
+                Control Unit: It controls and coordinates the functioning of all parts of computer. It does not involve in processing and storing data.
+                
+                ALU: It performs arithmetic and logical functions. Arithmetic functions include addition, subtraction, multiplication and division. Logical functions mainly include selecting, comparing and merging the data.
+                
+                
+        Hardware
+                
+        parts of a computer that you touch
+        
+            Motherboard - thin piece of equipment that holds the computer together
+                          allocates power to all pieces
+                          allows the pieces to communciate with each other
+                          
+            
+        Software
+        
+            a collection of data and instructions that the computer follows to do its tasks
+            
+            programming language - a way for users to understand and control what the hardware of a machine does
+            
+            data- a collection of energy patterns that represent objects that the machine can use in order to do its tasks
+            
+            program - another collection of energy patters that the machine takes as instruction to transport or change whatever data it is already working with, or make alterations to itself with the same or different energy
+            
+            
+    Computer Memory
+    
+         The computer memory is divided into large number of small parts known as cells. Each cell has a unique address which varies from 0 to memory size minus one.
+         
+         Computer memory is of two types: Volatile (RAM) and Non-volatile (ROM). The secondary memory (hard disk) is referred as storage not memory.
+         
+         
+         however based on spatial reference we have four types
+         
+         Register memory - hold in register found in CPU, holds data, instructions and memory addreses, that the CPU needs and users work with it using Aseembly Languages such as MIPS
+    
+        
+                
