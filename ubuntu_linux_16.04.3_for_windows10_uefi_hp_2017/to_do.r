706cad950a8f8883ab0e6af34fcdcc7a39f7dd82
Michael Odumosu <michaelodumosu57@gmail.com>
2018-01-14 11:57:11 -0500

    got API I worked on to get going and run off of elgesis

--- ubuntu_linux_16.04.3_for_windows10_uefi_hp_2017/to_do.r
+++ ubuntu_linux_16.04.3_for_windows10_uefi_hp_2017/to_do.r
@@ -1,8 +1,6 @@
 whats next
 
 learn how to access the root folder
-get git running on your laptop
-get all current browsers running
-pull everything from your computer and your computer on email
-
-disconnect your albany email from everything including git hub
+learn more about tunnelbear
+learn more about freshbooks
+learn more about synergy
